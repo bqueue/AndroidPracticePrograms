32ef4139b5d7f27d49e096d09bccd9f6a4d80902
Brian Quock <bquock@usc.edu>
2015-12-08 17:58:49 -0800

    Added basic async download code.  Throwing error because permissions needed first.

--- Top10Downloader/app/src/main/java/com/quockworks/top10downloader/MainActivity.java
+++ Top10Downloader/app/src/main/java/com/quockworks/top10downloader/MainActivity.java
@@ -1,12 +1,21 @@
 package com.quockworks.top10downloader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String top10Url = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute(top10Url);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +57,60 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // 3 parameters: 1) download location, 2) progress bar, 3) resultant data type
+    private class DownloadData extends AsyncTask<String, Void, String>{
+
+        private String mFileContents;
+
+        // Run without blocking
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+            if(mFileContents == null){
+                Log.d("DownloadData", "Error downloading");
+            }
+            return mFileContents;
+        }
+
+        private String downloadXMLFile(String urlPath){
+            StringBuilder tempBuffer = new StringBuilder();
+            try{
+                // Try opening an http url connection
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", "The response code was " + response);
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                /* Download data */
+                int charRead;
+                char[] inputBuffer = new char[500]; // Allocate space to read file 500 bytes at a time
+                while(true){
+                    charRead = isr.read(inputBuffer);   // try to read data into inputBuffer, charRead is the number of characters read
+                    if(charRead <= 0){
+                        break;
+                    }
+
+                    // Store characters from buffer, append to the temp buffer
+                    tempBuffer.append(String.copyValueOf(inputBuffer),0,charRead);
+                }
+                return tempBuffer.toString();
+
+            } catch (IOException e){
+                Log.d("DownloadData", "IO Exception reading data: " + e.getMessage());
+            } catch (SecurityException e){
+                Log.d("DownloadData", "Security Exception. Needs permissions? " + e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("DownloadData", "Result was " + result);
+        }
     }
 }

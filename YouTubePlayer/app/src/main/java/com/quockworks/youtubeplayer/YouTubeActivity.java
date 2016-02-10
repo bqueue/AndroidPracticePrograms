cd9c143bc64f3442020f5590e696cecbdebec8fc
Brian Quock <bquock@usc.edu>
2016-02-09 19:24:56 -0800

    Added basic YouTube player functionality

--- YouTubePlayer/app/src/main/java/com/quockworks/youtubeplayer/YouTubeActivity.java
+++ YouTubePlayer/app/src/main/java/com/quockworks/youtubeplayer/YouTubeActivity.java
@@ -1,16 +1,23 @@
 package com.quockworks.youtubeplayer;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+/*
+* YouTube API documentation:
+* https://developers.google.com/youtube/android/player/reference/com/google/android/youtube/player/package-summary
+* This activity will only run on a device unless the Google Play service and YouTube app are downloaded and installed on your emulator.
+* */
+
 public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
 
-    private String GOOGLE_API_KEY = "TBA";
-    private String YOUTUBE_VIDEO_ID = "TBA";
+    private String GOOGLE_API_KEY = "AIzaSyB4xh2zIJ5nUfytHeCb6huCQk-S_jvAHyo";  // Substitute your real API key here
+    private String YOUTUBE_VIDEO_ID = "FyASdjZE0R0";                            // The Chainsmokers - Roses (Audio) ft. ROZES
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +39,79 @@ public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlaye
 //        });
     }
 
+    // Set up listeners and cue the video
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        Toast.makeText(this, "Initialized YouTube Player successfully", Toast.LENGTH_LONG).show();
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
 
+        if(!wasRestored){
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
     }
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+            Toast.makeText(YouTubeActivity.this, "Good, video is playing ok", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onPaused() {
+            Toast.makeText(YouTubeActivity.this, "Video has paused", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+            Toast.makeText(YouTubeActivity.this, "Click Ad now, make the video creator rich!", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoStarted() {
+            Toast.makeText(YouTubeActivity.this, "Video has started", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
+        Toast.makeText(this, "Failed to initialize YouTube Player", Toast.LENGTH_LONG).show();
     }
 }
